761644272c1a510f08fa47977c67a45b12f11469
ashtonmaster77 <111986965+ashtonmaster77@users.noreply.github.com>
2023-04-18 11:31:05 +0800

    Add files via upload

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+// Copyright 2022 Ashton (ashtonmaster77)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_Z,    KC_X,    KC_C     
+    ),
+
+};
