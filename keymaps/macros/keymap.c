5033cecdbd5eb2105795257fdd3246e81ef7e6b3
ashtonmaster77 <111986965+ashtonmaster77@users.noreply.github.com>
2023-04-25 10:36:13 +0800

    Update keymap.c

--- keymaps/macros/keymap.c
+++ keymaps/macros/keymap.c
@@ -2,7 +2,22 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-
+enum custom_keycodes {
+    AMOGUS = SAFE_RANGE,
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case AMOGUS:
+        if (record->event.pressed) {
+            // when keycode AMOGUS is pressed
+            SEND_STRING("Hello!"SS_tap(X_ENTER);
+        } else {
+            // when keycode AMOGUS is released
+        }
+        break;
+    }
+    return true;
+};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
